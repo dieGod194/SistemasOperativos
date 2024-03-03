ac47e5c18ad9e4da8496aa115e5a562c184e246e
Diego Díaz <122294998+dieGod194@users.noreply.github.com>
2024-03-02 23:26:22 -0600

    Add files via upload

---
+++ EjercicioLectura/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+    float fd;
+    int m;
+    int i;
+    int valuesR[5];
+
+    fd= open("C:\\Users\\Jugge\\CLionProjects\\EjerciciosError\\cmake-build-debug\\datos.txt", 0);
+
+    m = read(fd, valuesR, sizeof(valuesR));
+
+    printf("Valores leidos: \n");
+
+    for(i = 0; i < 5; i++) {
+        printf("%f\n", &valuesR[i]);
+    }
+
+    close(fd);
+
+    return 0;
+}
---
+++ EjercicioLecturaPrintN/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int error_comp(int fd, int n) {
+    if (fd < 0) {
+        if (errno == EAGAIN) {
+            printf("Archivo bloqueado.\n");
+            exit(1);
+        } else if (errno == EACCES) {
+            printf("Problema de permisos del archivo.\n");
+            exit(1);
+        } else if (errno == EBADF) {
+            printf("Mal descriptor del archivo.\n");
+            exit(1);
+        } else {
+            printf("Error desconocido.\n");
+            exit(1);
+        }
+    }
+}
+
+int main() {
+    float fd;
+    int n;
+    int i;
+    int values[5];
+
+    fd = creat("datos2.txt", 777);
+
+    error_comp(fd, n);
+
+    printf("Ingresa los 5 numeros que quieres escribir en tu txt\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%f", &values[i]);
+        n = write(fd, &values[i], sizeof(float));
+        error_comp(fd, n);
+
+        printf("n = %d\n", n);
+    }
+
+
+    close(fd);
+
+    return 0;
+}
